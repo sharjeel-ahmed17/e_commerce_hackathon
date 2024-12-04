675b76a99996358eb2bb045a7196de85e1e72f1e
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-12-04 20:13:43 +0500

    code push to github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const Home = () => {
       <h1 className="">shad cn button component</h1>
       <p className="font-poppins text-white">sharjeel</p>
       <p className="font-inter text-white">sharjeel</p>
-      <p className=" text-white">sharjeel</p>
+      <p className=" text-white bg-btn">sharjeel</p>
       {/* <p>sharjeel</p> */}
       <Button>click me</Button>
     </div>
